83d03e6f5edb2c06a8061837418312eafcb5159c
David Garcia Martinez <dgarmar@gmail.com>
2020-10-20 14:54:06 +0200

    Refs #34 Refs #21 Incluidos datos de prueba en index.test.js y eliminados de index.js

---
+++ test/index.test.js
@@ -0,0 +1,6 @@
+var expect = require("chai").expect;
+const Alojamiento = require("./alojamiento.js");
+const Punto_interes = require("./punto_interes.js");
+const Transporte = require("./transporte.js");
+const Destino = require("./destino.js");
+const Index = require('../src/index.js');
