e622aa0f7a0c71dbbd28da45719ef6cbc4ab0a4b
David Garcia Martinez <dgarmar@gmail.com>
2020-10-20 14:47:39 +0200

    Refs #33 Añadida tarea para ejecutar los tests unitarios

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
+var mocha = require('gulp-mocha');
 
-
-gulp.task('default', function(){
-  console.log('Hello world!');
+/** Tarea que se encarga de ejecutar los tests unitarios */
+gulp.task('test', async function(){
+  gulp.src(['test/*.js'])
+  .pipe(mocha());
 });
