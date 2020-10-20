4f9b9dc1c5474c76e60c466bb688c6613ce02a45
David Garcia Martinez <dgarmar@gmail.com>
2020-10-20 22:38:35 +0200

    Refs #33 Añadida la tarea encargada de instalar las dependencias necesarias a gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var run = require('gulp-run');
+
+/** Tarea que se encarga de instalar las dependencias */
+gulp.task('install', async function(){
+  gulp.src(['./'])
+  .pipe(run('npm install'))
+});
 
 /** Tarea que se encarga de ejecutar los tests unitarios */
 gulp.task('test', async function(){
