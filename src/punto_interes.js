d883ae9a8f03e3a162ae96042f9cd0db5ea2c73d
David Garcia Martinez <dgarmar@gmail.com>
2020-11-01 19:21:06 +0100

    Closes #84 Refs #6 Refs #48 Refs #81 Añadido el atributo precio a los puntos de interés

--- src/punto_interes.js
+++ src/punto_interes.js
@@ -7,13 +7,15 @@ class Punto_interes{
    * @param {String} localizacion - Nombre del destino en el que se encuentra el punto de interés
    * @param {String} coordenadas - Coordenadas geográficas del punto de interés
    * @param {Number} valoracion - Media de las puntuaciones que los usuarios les han otorgado a este punto de interés
+   * @param {Number} precio - Precio de la reserva en el punto de interés
    */
-  constructor(nombre, descripcion, localizacion, coordenadas, valoracion){
+  constructor(nombre, descripcion, localizacion, coordenadas, valoracion, precio = 0){
     this.nombre = nombre;
     this.descripcion = descripcion;
     this.localizacion = localizacion;
     this.coordenadas = coordenadas;
     this.valoracion = valoracion;
+    this.precio = precio;
   }
 
   /**
@@ -40,29 +42,37 @@ class Punto_interes{
     return this.valoracion;
   }
 
+  get _precio(){
+    return this.precio;
+  }
+
   /**
    * Métodos set de la clase Punto_interes
    */
 
-   set _nombre(nuevo_nombre){
-     this.nombre = nuevo_nombre;
-   }
+  set _nombre(nuevo_nombre){
+    this.nombre = nuevo_nombre;
+  }
 
-   set _descripcion(nueva_descripcion){
-     this.descripcion = nueva_descripcion;
-   }
+  set _descripcion(nueva_descripcion){
+    this.descripcion = nueva_descripcion;
+  }
 
-   set _localizacion(nueva_localizacion){
-     this.localizacion = nueva_localizacion;
-   }
+  set _localizacion(nueva_localizacion){
+    this.localizacion = nueva_localizacion;
+  }
 
-   set _coordenadas(nuevas_coordenadas){
-     this.coordenadas = nuevas_coordenadas;
-   }
+  set _coordenadas(nuevas_coordenadas){
+    this.coordenadas = nuevas_coordenadas;
+  }
 
-   set _valoracion(nueva_valoracion){
-     this.valoracion = nueva_valoracion;
-   }
+  set _valoracion(nueva_valoracion){
+    this.valoracion = nueva_valoracion;
+  }
+
+  set _precio(nuevo_precio){
+    this.precio = nuevo_precio;
+  }
 }
 
 module.exports = Punto_interes;
