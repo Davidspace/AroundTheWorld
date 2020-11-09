aa4547f85f14e427faae9c94fe240a3cf543fcac
David Garcia Martinez <dgarmar@gmail.com>
2020-11-09 21:19:12 +0100

    Refs #111 Modificado gulpfile.js [skip travis]

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var run = require('gulp-run');
 
 /** Tarea que se encarga de instalar las dependencias */
 gulp.task('install', async function(){
-  gulp.src(['./'])
+  gulp.src(['./package.json'])
   .pipe(run('npm install'))
 });
 
