b0c80a896ebf5b20369a692892503ff63ae8b19e
David Garcia Martinez <dgarmar@gmail.com>
2020-11-09 21:27:55 +0100

    Refs #111 Modificado gulpfile.js y .shippable.yml [skip travis]

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var run = require('gulp-run');
 
 /** Tarea que se encarga de instalar las dependencias */
 gulp.task('install', async function(){
-  gulp.src(['./package.json'])
+  gulp.src(['./'])
   .pipe(run('npm install'))
 });
 
