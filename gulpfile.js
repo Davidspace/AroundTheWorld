1be44cd26599978eb0a335bb7e584e9097d7a000
David Garcia Martinez <dgarmar@gmail.com>
2020-11-10 11:43:11 +0100

    Refs #111 Nuevas instalaciones [skip travis]

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
-var gulp = require('gulp');
-var mocha = require('gulp-mocha');
-var run = require('gulp-run');
+var gulp = require('/root/.nvm/versions/node/v14.13.0/lib/node_modules/gulp');
+var mocha = require('/root/.nvm/versions/node/v14.13.0/lib/node_modules/gulp-mocha');
+var run = require('/root/.nvm/versions/node/v14.13.0/lib/node_modules/gulp-run');
 
 /** Tarea que se encarga de instalar las dependencias */
 gulp.task('install', async function(){
-  gulp.src(['/home/david/.nvm/versions/node/v14.13.0/lib'])
+  gulp.src(['./'])
   .pipe(run('npm install'))
 });
 
