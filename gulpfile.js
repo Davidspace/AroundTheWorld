8eb9fc8d3602fb2ab6383a8434bb932148caa561
David Garcia Martinez <dgarmar@gmail.com>
2020-11-10 01:46:22 +0100

    Refs #111 Nuevas instalaciones [skip travis]

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var run = require('gulp-run');
 
 /** Tarea que se encarga de instalar las dependencias */
 gulp.task('install', async function(){
-  gulp.src(['./'])
+  gulp.src(['./node_modules'])
   .pipe(run('npm install'))
 });
 
