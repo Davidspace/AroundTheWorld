d88873abec18450ce9b5416eb969cc56dac63bc5
David Garcia Martinez <dgarmar@gmail.com>
2020-11-10 11:38:26 +0100

    Refs #111 Nuevas instalaciones

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var run = require('gulp-run');
 
 /** Tarea que se encarga de instalar las dependencias */
 gulp.task('install', async function(){
-  gulp.src(['./'])
+  gulp.src(['/home/david/.nvm/versions/node/v14.13.0/lib'])
   .pipe(run('npm install'))
 });
 
