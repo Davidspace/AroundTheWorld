82701446febddea1ad00f885d2d0fafa2a4dd6b8
David Garcia Martinez <dgarmar@gmail.com>
2020-11-10 11:55:51 +0100

    Refs #111 Nueva ruta para los módulos [skip travis]

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,10 @@
-var gulp = require('/root/.nvm/versions/node/v14.13.0/lib/node_modules/gulp');
-var mocha = require('/root/.nvm/versions/node/v14.13.0/lib/node_modules/gulp-mocha');
-var run = require('/root/.nvm/versions/node/v14.13.0/lib/node_modules/gulp-run');
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+var run = require('gulp-run');
 
 /** Tarea que se encarga de instalar las dependencias */
 gulp.task('install', async function(){
   gulp.src(['./'])
-  .pipe(preprocess({context: { NODE_PATH: '$NODE_PATH:/root/.nvm/versions/node/v14.13.0/lib/node_modules'}}))
   .pipe(run('npm install'))
 });
 
