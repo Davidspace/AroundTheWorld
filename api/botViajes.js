f9ab99830c06693b332f091ddb7760f3a69086ab
David Garcia Martinez <dgarmar@gmail.com>
2020-11-24 23:07:23 +0100

    Refs #127 Refs #129 Desarrollado funcionamiento del bot

--- api/botViajes.js
+++ api/botViajes.js
@@ -28,103 +28,100 @@ function viajesUsuario(username, password){
 
 module.exports = async (req, res) => {
   if (req.body != undefined){
-    if (req.body.message.text != undefined){
-      // Obtenemos el ID del chat de Telegram desde el que proviene el mensaje
-      var chatID = req.body.message.from.id;
+    // Obtenemos el ID del chat de Telegram desde el que proviene el mensaje
+    var chatID = req.body.message.chat.id;
 
-      // Obtenemos el contenido del mensaje
-      var text = req.body.message.text;
+    // Obtenemos el contenido del mensaje
+    var text = req.body.message.text;
 
-      username = "";
+    username = "";
 
-      password = "";
+    password = "";
 
-      mensaje = "";
+    mensaje = "";
 
-      if (text == '/start'){
-        mensaje = '¡Hola! Este bot ha sido desarrollado con el propósito de mostrar a cada usuario\
-          de la aplicación la lista de viajes con los que cuentan. Comienza identificándote mediante\
-          tu nombre de usuario de la siguiente manera: Username: <username>. \n\nUsa el comando /help\
-          para conocer todos los comandos disponibles.'
-      }
-
-      if (text == 'help'){
-        mensaje = 'Debe iniciar sesión indicando sus credenciales primero mediante Username: <username> y\
-          y después con Password: <password>. Si ya lo ha hecho, puede listar sus viajes mediante el\
-          comando /viajes.'
-      }
-
-      usuario_index = -1;
-
-      if (text.startsWith('Username:')){
-        username = text.split(" ", 2);
-
-        username = nickname.split(",").pop();
-
-        usuario_existe = False;
-        
-        for (i = 0; i < usuarios.length && !usuario_existe; i++){
-          if (usuarios[i]['username'] === username){
-            usuario_existe = True;
-
-            usuario_index = i;
-          }
-        }
-
-        if (usuario_existe){
-          mensaje = 'Ahora, introduce tu contraseña de la siguiente manera: Password: <password>'
-        }
-
-        else{
-          username = "";
-
-          mensaje = 'El usuario que has indicado no existe. Inténtalo de nuevo.'
-        }
-      }
-
-      if (text.startsWith('Password:')){
-        password = text.split(" ", 2);
-
-        password = password.split(",").pop();
-
-        password_correcta = False;
-
-        if (usuario_index != -1){
-          if (usuarios[usuario_index]['password'] === password){
-            password_correcta = True;
-          }
-        }
-
-        else{
-          password = "";
-
-          mensaje = 'Aún no has introducido un usuario correcto.'
-        }
-
-        if (usuario_index != -1){
-          if (password_correcta){
-            mensaje = 'Te has identificado correctamente. Usa el comando /viajes para listar tus viajes.'
-          }
-
-          else{
-            mensaje = 'La contraseña es incorrecta. Inténtalo de nuevo.'
-          }
-        }
-      }
-
-      if (text == '/viajes'){
-        viajesUsuario(username, password);
-      }
-
-      var res_json = {text: mensaje, method: "sendMessage",  chat_id: chatID};
-      res.setHeader("Content-Type","application/json");
-      res.status(200).json(res_json);
+    if (text == '/start'){
+      mensaje = '¡Hola! Este bot ha sido desarrollado con el propósito de mostrar a cada usuario\
+        de la aplicación la lista de viajes con los que cuentan. Comienza identificándote mediante\
+        tu nombre de usuario de la siguiente manera: Username: <username>. \n\nUsa el comando /help\
+        para conocer todos los comandos disponibles.'
     }
+
+    if (text == 'help'){
+      mensaje = 'Debe iniciar sesión indicando sus credenciales primero mediante Username: <username> y\
+        y después con Password: <password>. Si ya lo ha hecho, puede listar sus viajes mediante el\
+        comando /viajes.'
+    }
+
+    usuario_index = -1;
+
+    if (text.startsWith('Username:')){
+      username = text.split(" ", 2);
+
+      username = nickname.split(",").pop();
+
+      usuario_existe = False;
+        
+      for (i = 0; i < usuarios.length && !usuario_existe; i++){
+        if (usuarios[i]['username'] === username){
+          usuario_existe = True;
+
+          usuario_index = i;
+        }
+      }
+
+      if (usuario_existe){
+        mensaje = 'Ahora, introduce tu contraseña de la siguiente manera: Password: <password>'
+      }
+
+      else{
+        username = "";
+
+        mensaje = 'El usuario que has indicado no existe. Inténtalo de nuevo.'
+      }
+    }
+
+    if (text.startsWith('Password:')){
+      password = text.split(" ", 2);
+
+      password = password.split(",").pop();
+
+      password_correcta = False;
+
+      if (usuario_index != -1){
+        if (usuarios[usuario_index]['password'] === password){
+          password_correcta = True;
+        }
+      }
+
+      else{
+        password = "";
+
+        mensaje = 'Aún no has introducido un usuario correcto.'
+      }
+
+      if (usuario_index != -1){
+        if (password_correcta){
+          mensaje = 'Te has identificado correctamente. Usa el comando /viajes para listar tus viajes.'
+        }
+
+        else{
+          mensaje = 'La contraseña es incorrecta. Inténtalo de nuevo.'
+        }
+      }
+    }
+
+    if (text == '/viajes'){
+      viajesUsuario(username, password);
+    }
+
+    var res_json = {text: mensaje, method: "sendMessage",  chat_id: chatID};
+    res.setHeader("Content-Type","application/json");
+    res.status(200).json(res_json);
   }
 
   else{
     res.setHeader("Content-Type","text/plain");
-    res.status(200).send('Esto es una función desplegada para un bot de Telegram\
-      llamado @viajesAroundTheWorld_bot');
+    res.status(200).send('Iniciando el bot del proyecto AroundTheWorld');
   }
 }
