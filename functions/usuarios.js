baff9543547bf5190ea334f0ca41706b2283a88b
David Garcia Martinez <dgarmar@gmail.com>
2020-11-25 20:14:28 +0100

    Closes #126 Refs #128 Desarrollada función serverless desplegada en Netlify

--- functions/usuarios.js
+++ functions/usuarios.js
@@ -2,21 +2,31 @@ const usuarios = require('./usuarios.json');
 
 exports.handler = async function(event, context){
   if (usuarios.length > 0){
-    var mensaje = "Los usuarios registrados en el sistema son los siguientes:\n\n";
+    var mensaje = "["
+
+    var num_mensajes = 0;
 
     for (var i = 0; i < usuarios.length; i++){
-      mensaje += "Destino: " + usuarios[i]['nombre'] + "\n" +
-        "Apellidos: " + usuarios[i]['apellidos'] + "\n" +
-        "Email: " + usuarios[i]['email'] + "\n" +
-        "Username: " + usuarios[i]['username'] + "\n" +
-        "Password" + usuarios[i]['password'] + "\n" +
-        "Dirección: " + usuarios[i]['direccion'] + "\n" +
-        "Teléfono: " + usuarios[i]['telefono'] + "\n";
+      mensaje += "{destino: " + usuarios[i]['nombre'] + ", " +
+        "apellidos: " + usuarios[i]['apellidos'] + ", " +
+        "email: " + usuarios[i]['email'] + ", " +
+        "username: " + usuarios[i]['username'] + ", " +
+        "password" + usuarios[i]['password'] + ", " +
+        "direccion: " + usuarios[i]['direccion'] + ", " +
+        "telefono: " + usuarios[i]['telefono'] + "}";
+
+      if (num_mensajes != usuarios.length - 1){
+        mensaje += ", ";
+
+        num_mensajes += 1;
+      }
     }
+
+    mensaje += "]";
   }
 
   else{
-    mensaje = "Aún no existen usuarios registrados en la aplicación";
+    mensaje = "{mensaje: Aún no existen usuarios registrados en la aplicación}";
   }
 
   return{
