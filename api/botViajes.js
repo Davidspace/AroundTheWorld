f6172efd84a2d1e34c61809a0158e4709a3cf70e
David Garcia Martinez <dgarmar@gmail.com>
2020-11-25 13:41:44 +0100

    Refs #127 Refs #129 Desarrollando funcionamiento del bot [skip travis]

--- api/botViajes.js
+++ api/botViajes.js
@@ -4,27 +4,31 @@ const viajes = require('./viajes.json')
 function viajesUsuario(username, password){
   var mensaje = "";
 
-  mensaje += "Tus viajes son: ";
+  if (username !== "" && password !== ""){
+    mensaje += "Tus viajes son: ";
 
-  var existen_viajes = -1;
+    var existen_viajes = -1;
             
-  for (i = 0; i < viajes.length; i++){
-    if (viajes[i]['usuario'] === username){
-      mensaje += "\n\nUsuario: " + viajes[i]['usuario'] + "\n" +
-        "Nombre del destino: " + viajes[i]['nombre_destino'] + "\n" +
-        "Alojamientos: " + viajes[i]['alojamientos'] + "\n" +
-        "Puntos de interés: " + viajes[i]['puntos_interes'] + "\n" +
-        "Transportes: " + viajes[i]['transportes'] + "\n" + 
-        "Precio: " + viajes[i]['precio'];
+    for (i = 0; i < viajes.length; i++){
+      if (viajes[i]['usuario'] === username){
+        mensaje += "\n\nUsuario: " + viajes[i]['usuario'] + "\n" +
+          "Nombre del destino: " + viajes[i]['nombre_destino'] + "\n" +
+          "Alojamientos: " + viajes[i]['alojamientos'] + "\n" +
+          "Puntos de interés: " + viajes[i]['puntos_interes'] + "\n" +
+          "Transportes: " + viajes[i]['transportes'] + "\n" + 
+          "Precio: " + viajes[i]['precio'];
 
-      if (existen_viajes == -1){
         existen_viajes = 1;
       }
     }
+
+    if (existen_viajes == -1){
+      mensaje += "¡ninguno! ¿No has pensado en darte un capricho?";
+    }
   }
 
-  if (existen_viajes == -1){
-    mensaje += "¡ninguno! ¿No has pensado en darte un capricho?";
+  else{
+    mensaje = 'Aún no se ha identificado.';
   }
 
   return mensaje;
@@ -60,37 +64,73 @@ module.exports = async (req, res) => {
 
     if (text.startsWith('Username:')){
       username = text.split(" ")[1];
+
+      var usuario_existe = -1;
         
-      for (i = 0; i < usuarios.length && usuario_index == -1; i++){
+      for (i = 0; i < usuarios.length && usuario_existe == -1; i++){
         if (usuarios[i]['username'] === username){
+          usuario_existe = 1;
+
           usuario_index = i;
         }
       }
 
-      if (usuario_index != -1){
+      if (usuario_existe == 1){
         password = text.split(" ").pop();
 
         var password_correcta = -1;
 
-        if (usuarios[usuario_index]['password'] === password){
-          password_correcta = 1;
-        }
-          
-        if (password_correcta == 1){
-          mensaje = 'Te has identificado correctamente. Tus viajes, en el caso de que existan, van a ser ' + 
-            'listados a continuación.\n\n';
-
-          mensaje += viajesUsuario(username, password);
+        if (usuario_index != -1){
+          if (usuarios[usuario_index]['password'] === password){
+            password_correcta = 1;
+          }
         }
 
-        else{
-          mensaje = 'La contraseña es incorrecta. Inténtalo de nuevo.';
+        if (usuario_index != -1){
+          if (password_correcta == 1){
+            mensaje = 'Te has identificado correctamente. Usa el comando /viajes para listar tus viajes.';
+          }
+
+          else{
+            mensaje = 'La contraseña es incorrecta. Inténtalo de nuevo.';
+          }
         }
       }
 
       else{
         mensaje = 'El usuario que has indicado no existe. Inténtalo de nuevo.';
       }
+    }
+
+    if (text.startsWith("Password:")){
+      password = text.split(" ").pop();
+
+      var password_correcta = -1;
+
+      if (usuario_index != -1){
+        if (usuarios[usuario_index]['password'] === password){
+          password_correcta = 1;
+        }
+      }
+
+      else{
+        password = "";
+        mensaje = 'Aún no has introducido un usuario correcto.';
+      }
+
+      if (usuario_index != -1){
+        if (password_correcta == 1){
+          mensaje = 'Te has identificado correctamente. Usa el comando /viajes para listar tus viajes.';
+        }
+
+        else{
+          mensaje = 'La contraseña es incorrecta. Inténtalo de nuevo.';
+        }
+      }
+    }
+
+    if (text == "/viajes"){
+      mensaje = viajesUsuario(username, password);
     }
 
     var res_json = {text: mensaje, method: "sendMessage", chat_id: chatID};
