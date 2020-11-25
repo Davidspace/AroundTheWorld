c4dadeb67cb73cc0c84d0c9a8bee95565b64763f
David Garcia Martinez <dgarmar@gmail.com>
2020-11-25 15:14:53 +0100

    Refs #127 Refs #129 Desarrollando funcionamiento del bot [skip travis]

--- api/botViajes.js
+++ api/botViajes.js
@@ -4,19 +4,19 @@ const viajes = require('./viajes.json')
 function viajesUsuario(username){
   var mensaje = "";
 
-  mensaje += "Tus viajes son:";
+  mensaje += "Tus viajes son: ";
 
   var i, j = 0;
             
   for (i = 0; i < viajes.length; i++){
     if (viajes[i]['usuario'] === username){
       mensaje += "\n\nUsuario: " + viajes[i]['usuario'] + "\n" +
-        "Nombre del destino: " + viajes[i]['nombre_destino'] + "\n";
+        "Nombre del destino: " + viajes[i]['nombre_destino'] + "\n" +
         "Alojamientos:\n";
 
-      for (j = 0; j < viajes[i]['alojamientos'].length; j++){
-        mensaje += viajes[i]['alojamientos'][j] + "\n";
-      }
+      viajes[i]['alojamientos'].forEach(function(obj){
+        mensaje += obj + "\n";
+      });
 
       mensaje += "Puntos de interés:\n";
 
@@ -62,42 +62,49 @@ module.exports = async (req, res) => {
         'le serán mostrados sus viajes.';
     }
 
-    if (text == "/help"){
-      mensaje = 'Debe iniciar sesión indicando sus credenciales mediante Username: <username> ' +
-        'Password: <password>. Automáticamente después se le mostrarán sus viajes.';
-    }
-
-    var usuario_index = -1;
-
-    if (text.startsWith('Username:')){
-      username = text.split(" ")[1];
-        
-      for (i = 0; i < usuarios.length && usuario_index == -1; i++){
-        if (usuarios[i]['username'] === username){
-
-          usuario_index = i;
-        }
-      }
-
-      if (usuario_index != -1){
-        password = text.split(" ").pop();
-
-        if (usuarios[usuario_index]['password'] === password){
-          mensaje = 'Te has identificado correctamente. A continuación se van a listar tus viajes ' +
-            'en el caso de que cuentes con alguno.\n\n';
-
-          mensaje += viajesUsuario(username);
-        }
-
-        else{
-          mensaje = 'La contraseña es incorrecta. Inténtalo de nuevo.';
-        }
+    else{
+      if (text == "/help"){
+        mensaje = 'Debe iniciar sesión indicando sus credenciales mediante Username: <username> ' +
+          'Password: <password>. Automáticamente después se le mostrarán sus viajes.';
       }
 
       else{
-        mensaje = 'El usuario que has indicado no existe. Inténtalo de nuevo.';
-      }
-    }
+        if (text.startsWith('Username:')){
+          var usuario_index = -1;
+
+          username = text.split(" ")[1];
+            
+          for (i = 0; i < usuarios.length && usuario_index == -1; i++){
+            if (usuarios[i]['username'] === username){
+
+              usuario_index = i;
+            }
+          }
+
+          if (usuario_index != -1){
+            password = text.split(" ").pop();
+
+            if (usuarios[usuario_index]['password'] === password){
+              mensaje = 'Te has identificado correctamente. A continuación se van a listar tus viajes ' +
+                'en el caso de que cuentes con alguno.\n\n';
+
+              mensaje += viajesUsuario(username);
+            }
+
+            else{
+              mensaje = 'La contraseña es incorrecta. Inténtalo de nuevo.';
+            }
+          }
+
+          else{
+            mensaje = 'El usuario que has indicado no existe. Inténtalo de nuevo.';
+          }
+        }
+
+        else{
+          mensaje = 'Este mensaje no corresponde a ninguna acción. Utiliza el comando /help ' + 
+            'para saber qué debes de hacer.';
+        }
 
     var res_json = {text: mensaje, method: "sendMessage", chat_id: chat_id};
     res.setHeader("Content-Type","application/json");
