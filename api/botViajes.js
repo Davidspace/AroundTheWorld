57e187245353dd086b5e6fb22e0c9885d7aba9ec
David Garcia Martinez <dgarmar@gmail.com>
2020-11-25 14:33:55 +0100

    Refs #127 Refs #129 Desarrollando funcionamiento del bot [skip travis]

--- api/botViajes.js
+++ api/botViajes.js
@@ -42,73 +42,66 @@ function viajesUsuario(username){
 }
 
 module.exports = async (req, res) => {
-  if (req.body != undefined){
-    if (req.body.message != undefined){
-      // Obtenemos el ID del chat de Telegram desde el que proviene el mensaje
-      var chat_id = req.body.message.chat.id;
+  if (req.body.message != undefined){
+    // Obtenemos el ID del chat de Telegram desde el que proviene el mensaje
+    var chat_id = req.body.message.from.id;
 
-      // Obtenemos el contenido del mensaje
-      var text = req.body.message.text;
+    // Obtenemos el contenido del mensaje
+    var text = req.body.message.text;
 
-      var username = "";
+    var username = "";
 
-      var password = "";
+    var password = "";
 
-      var mensaje = "";
+    var mensaje = "";
 
-      if (text == "/start"){
-        mensaje = '¡Hola! Este bot ha sido desarrollado con el propósito de mostrar a cada usuario ' +
-          'de la aplicación la lista de viajes con los que cuentan. Para ello, identifiquese de la ' +
-          'siguiente manera: Username: <username> Password: <password>. Si sus credenciales son correctos, ' +
-          'le serán mostrados sus viajes.';
-      }
+    if (text == "/start"){
+      mensaje = '¡Hola! Este bot ha sido desarrollado con el propósito de mostrar a cada usuario ' +
+        'de la aplicación la lista de viajes con los que cuentan. Para ello, identifiquese de la ' +
+        'siguiente manera: Username: <username> Password: <password>. Si sus credenciales son correctos, ' +
+        'le serán mostrados sus viajes.';
+    }
 
-      if (text == "/help"){
-        mensaje = 'Debe iniciar sesión indicando sus credenciales mediante Username: <username> ' +
-          'Password: <password>. Automáticamente después se le mostrarán sus viajes.';
-      }
+    if (text == "/help"){
+      mensaje = 'Debe iniciar sesión indicando sus credenciales mediante Username: <username> ' +
+        'Password: <password>. Automáticamente después se le mostrarán sus viajes.';
+    }
 
-      var usuario_index = -1;
+    var usuario_index = -1;
 
-      if (text.startsWith('Username:')){
-        username = text.split(" ")[1];
-          
-        for (i = 0; i < usuarios.length && usuario_index == -1; i++){
-          if (usuarios[i]['username'] === username){
+    if (text.startsWith('Username:')){
+      username = text.split(" ")[1];
+        
+      for (i = 0; i < usuarios.length && usuario_index == -1; i++){
+        if (usuarios[i]['username'] === username){
 
-            usuario_index = i;
-          }
+          usuario_index = i;
         }
+      }
 
-        if (usuario_index != -1){
-          password = text.split(" ").pop();
+      if (usuario_index != -1){
+        password = text.split(" ").pop();
 
-          if (usuarios[usuario_index]['password'] === password){
-            mensaje = 'Te has identificado correctamente. A continuación se van a listar tus viajes ' +
-              'en el caso de que cuentes con alguno.\n\n';
+        if (usuarios[usuario_index]['password'] === password){
+          mensaje = 'Te has identificado correctamente. A continuación se van a listar tus viajes ' +
+            'en el caso de que cuentes con alguno.\n\n';
 
-            mensaje += viajesUsuario(username);
-          }
-
-          else{
-            mensaje = 'La contraseña es incorrecta. Inténtalo de nuevo.';
-          }
+          mensaje += viajesUsuario(username);
         }
 
         else{
-          mensaje = 'El usuario que has indicado no existe. Inténtalo de nuevo.';
+          mensaje = 'La contraseña es incorrecta. Inténtalo de nuevo.';
         }
       }
 
-      var res_json = {text: mensaje, method: "sendMessage", chat_id: chat_id};
-      res.setHeader("Content-Type","application/json");
-      res.status(200).json(res_json);
+      else{
+        mensaje = 'El usuario que has indicado no existe. Inténtalo de nuevo.';
+      }
     }
 
-    else{
-      res.setHeader("Content-Type","text/plain");
-      res.status(200).send('El mensaje está vacio');
-    }
+    var res_json = {text: mensaje, method: "sendMessage", chat_id: chat_id};
+    res.setHeader("Content-Type","application/json");
+    res.status(200).json(res_json);
   }
 
   else{
