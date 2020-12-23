5201096d84189bde965d0f421c0a2f9af4065cca
David Garcia Martinez <dgarmar@gmail.com>
2020-12-23 10:51:10 +0100

    Refs #139 Refs #143 Corregido el tipo de petición realizada

--- test/api.test.js
+++ test/api.test.js
@@ -11,17 +11,19 @@ describe("Testeando las rutas incluidas en la API de la aplicación", function()
     });
   });
 
-  describe("2. Testeando la obtención de los datos de un usuario concreto dado su username", function() {
+  describe("2. Testeando la obtención de los datos de un usuario concreto dado su username " + 
+    "(GET /usuarios/Davidspace)", function() {
     it("Comprobando que se obtienen los datos correctamente", function(done){
       request(app)
-      .put('/usuarios/Davidspace')
+      .get('/usuarios/Davidspace')
       .expect('Content-Type', /json/)
       .expect(200, done);
     });
 
-    it("Comprobando que se lanza un error avisando de que el username dado no está registrado", function(done){
+    it("Comprobando que se lanza un error avisando de que el username dado no está registrado " +
+      "(GET /usuarios/Davidspac)", function(done){
       request(app)
-      .put('/usuarios/Davidspac')
+      .get('/usuarios/Davidspac')
       .expect('Content-Type', /json/)
       .expect(404, done)
     });
