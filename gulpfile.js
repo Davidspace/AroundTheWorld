2b717641937943c5a20294f7bf16d691114d5f0d
David Garcia Martinez <dgarmar@gmail.com>
2020-12-29 11:19:51 +0100

    No instalo nada con gulp install, ya se instala en el Dockerfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,10 @@ var run = require('gulp-run');
 
 /** Tarea que se encarga de instalar las dependencias */
 gulp.task('install', async function(){
-  gulp.src(['./'])
-  .pipe(run('npm install'))
+  /*
+    gulp.src(['./'])
+    .pipe(run('npm install'))
+  */
 });
 
 /** Tarea que se encarga de ejecutar los tests unitarios */
