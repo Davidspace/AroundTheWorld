6f38b8f8008f37b5e50c79f20b03ffbd0ea7ffb4
David Garcia Martinez <dgarmar@gmail.com>
2021-01-04 00:23:35 +0100

    Refs #147 Refs #148 Refs #149 Refs #5 Añadidos métodos para devolver los alojamientos, puntos de interés, transportes y destinos registrados en la aplicación

--- api/model.js
+++ api/model.js
@@ -1,20 +1,60 @@
-const Usuario = require('../src/usuario.js')
+const Usuario = require('../src/usuario.js');
+const Alojamiento = require('../src/alojamiento.js');
+const Punto_interes = require('../src/punto_interes.js');
+const Transporte = require('../src/transporte.js');
+const Destino = require('../src/destino.js');
 
 class Model{
   constructor(){
-    this.usuarios = new Array(Usuario);
+    this.usuarios = [];
 
-    this.usuarios[0] = new Usuario('David', 'Garcia Martinez', 'dgarmar@gmail.com', 'Davidspace', 'password1', 
-      'Calle Antequera 38', '616087213');
+    this.usuarios.push(new Usuario('David', 'Garcia Martinez', 'dgarmar@gmail.com', 'Davidspace', 'password1', 
+      'Calle Antequera 38', '616087213'));
 
-    this.usuarios[1] = new Usuario('Lucia', 'Garcia Martinez', 'luciagm@gmail.com', 'Luciagm', 'password2', 
-      'Calle Antequera 38', '616087444');
+    this.usuarios.push(new Usuario('Lucia', 'Garcia Martinez', 'luciagm@gmail.com', 'Luciagm', 'password2', 
+      'Calle Antequera 38', '616087444'));
 
-    this.usuarios[2] = new Usuario('Alba', 'Logenso Magtines', 'albalm@gmail.com', 'Albamay', 'password3', 
-      'Calle Caniles, 12', '616123124');
+    this.usuarios.push(new Usuario('Alba', 'Logenso Magtines', 'albalm@gmail.com', 'Albamay', 'password3', 
+      'Calle Caniles, 12', '616123124'));
 
-    this.usuarios[3] = new Usuario('Porfi', 'Magtines Zola', 'pogggfirio@gmail.com', 'Pogfirio', 'password4', 
-      'Calle Meme, 80', '615095634');
+    this.usuarios.push(new Usuario('Porfi', 'Magtines Zola', 'pogggfirio@gmail.com', 'Pogfirio', 'password4', 
+      'Calle Meme, 80', '615095634'));
+
+    this.alojamientos = [];
+
+    this.alojamientos.push(new Alojamiento("Hostal David", "Increible alojamiento", "Hostal",
+      "Baza", "37.4888637, -2.7709805", "8.7", 40));
+      
+    this.alojamientos.push(new Alojamiento("Hotel Hoja", "Somos los mejores", "Hotel",
+      "Guadix", "37.3006914 ,-3.1351956", "9.1", 30));
+
+    this.puntos_interes = [];
+
+    this.puntos_interes.push(new Punto_interes("Plaza Mayor", "La más grande", "Baza",
+      "37.3023453 ,-3.0232736", "9.3"));
+                                               
+    this.puntos_interes.push(new Punto_interes("Plaza Menor", "No tan grande", "Guadix",
+      "37.3006662 ,-3.1452778", "8.5"));
+                                               
+    this.transportes = [];
+                                               
+    this.transportes.push(new Transporte("Alsama", "Buses mega rápidos", "Autobus", "Baza",
+      "9.7", "07:00", "23:00"));
+                                               
+      this.transportes.push(new Transporte("Guataxi", "Taxis para todos", "Taxi", "Guadix",
+      "9.7", "07:00", "23:00"));
+
+    this.destinos = [];
+
+    this.destinos.push(new Destino("Baza", "Baza es el mejor pueblo que existe", "España",
+      25000, "Bastetano", "www.baza.com", 9.2,
+      this.alojamientos[0], this.puntos_interes[0],
+      this.transportes[0]));
+      
+    this.destinos.push(new Destino("Guadix", "Guadix es el segundo mejor pueblo que existe", "España",
+      24000, "Accitano", "www.guadix.com", 9.1,
+      this.alojamientos[1], this.puntos_interes[1],
+      this.transportes[1]));
   }
 
   get_usuario(username){
@@ -102,6 +142,22 @@ class Model{
       }
     }
   }
+
+  get_alojamientos(){
+    return this.alojamientos;
+  }
+
+  get_puntos_interes(){
+    return this.puntos_interes;
+  }
+
+  get_transportes(){
+    return this.transportes;
+  }
+
+  get_destinos(){
+    return this.destinos;
+  }
 }
 
-module.exports = Model;
+module.exports = Model, Usuario;
